88ff622f31eb58de07a0ff5708d33e79e13ae23a
chenfule <1195289668@qq.com>
2019-11-05 00:22:38 +0800

    新增音乐的mapper类、service类、controller类

---
+++ src/main/java/com/yiguan/douban/controller/MusicController.java
@@ -0,0 +1,9 @@
+package com.yiguan.douban.controller;
+
+/**
+ * @author: chenfl
+ * @Date: 2019/11/5
+ * @Version: 1.0
+ */
+public class MusicController {
+}
---
+++ src/main/java/com/yiguan/douban/mapper/MusicMapper.java
@@ -0,0 +1,9 @@
+package com.yiguan.douban.mapper;
+
+/**
+ * @author:  chenfl
+ * @Date: 2019/11/5
+ * @Version: 1.0
+ */
+public interface MusicMapper {
+}
