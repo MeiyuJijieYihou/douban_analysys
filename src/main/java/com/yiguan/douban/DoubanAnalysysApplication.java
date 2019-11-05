ce9b6907696d48474db30cf4bdc0dc2bd5128966
Chris_Nai <201750080522@stu.csust.edu.cn>
2019-11-05 21:09:28 +0800

    加@EnableSwagger2

--- src/main/java/com/yiguan/douban/DoubanAnalysysApplication.java
+++ src/main/java/com/yiguan/douban/DoubanAnalysysApplication.java
@@ -2,10 +2,13 @@ package com.yiguan.douban;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.yiguan.douban.mapper")
+
+@EnableSwagger2
 public class DoubanAnalysysApplication {
 
 	public static void main(String[] args) {
