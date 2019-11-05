23d2a32fdcdb10413e15d60d893e05bf04c3ef3f
DX <184606905@qq.com>
2019-11-05 22:27:22 +0800

    实现Movie相关功能，注释Movie实体类。

--- src/main/java/com/yiguan/douban/entity/Movie.java
+++ src/main/java/com/yiguan/douban/entity/Movie.java
@@ -1,24 +1,88 @@
-package com.sample;
+package com.yiguan.douban.entity;
 
 import lombok.Data;
 
 @Data
 public class Movie {
 
+  /**
+   * 电影编号
+   */
   private Long mid;
+
+  /**
+   * 电影名称
+   */
   private String name;
+
+  /**
+   * 导演
+   */
   private String director;
+
+  /**
+   * 编剧
+   */
   private String screenwriter;
+
+  /**
+   * 主演
+   */
   private String starring;
+
+  /**
+   * 类型
+   */
   private String type;
+
+  /**
+   * 制片国家/地区
+   */
   private String countriesRegions;
+
+  /**
+   * 语言
+   */
   private String language;
+
+  /**
+   * 上映日期
+   */
   private java.sql.Timestamp date;
+
+  /**
+   * 片长
+   */
   private Long length;
+
+  /**
+   * 又名
+   */
   private String otherName;
+
+  /**
+   * IMDb
+   */
   private String imDb;
+
+  /**
+   * 短评数量
+   */
   private Long shortCommentNumber;
+
+  /**
+   * 影评数量
+   */
   private Long commentNumber;
+
+  /**
+   * 豆瓣评分
+   */
   private Double score;
+
+  /**
+   * 评分人数
+   */
+  private Integer scoreNumber;
 
 }
--- src/main/java/com/yiguan/douban/service/MovieService.java
+++ src/main/java/com/yiguan/douban/service/MovieService.java
@@ -1,8 +1,13 @@
 package com.yiguan.douban.service;
 
+import com.yiguan.douban.entity.Movie;
+
+import java.util.List;
+
 /**
  * @author abing
  * @create 2019/11/4 下午11:19
  */
 public interface MovieService {
+    List<Movie> findAllMovie();
 }
