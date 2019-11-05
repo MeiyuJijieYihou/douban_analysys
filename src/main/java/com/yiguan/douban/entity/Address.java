45fc50156de94b781eec02d40f82f858c5a732fe
Chris_Nai <201750080522@stu.csust.edu.cn>
2019-11-05 21:53:24 +0800

    user与address实体类注释

--- src/main/java/com/yiguan/douban/entity/Address.java
+++ src/main/java/com/yiguan/douban/entity/Address.java
@@ -5,8 +5,19 @@ import lombok.Data;
 @Data
 public class Address {
 
+  /**
+   *地址id
+   */
   private Long id;
+
+  /**
+   * 地址名字
+   */
   private String name;
+
+  /**
+   * 父地址id
+   */
   private Long fid;
 
 
--- src/main/java/com/yiguan/douban/entity/User.java
+++ src/main/java/com/yiguan/douban/entity/User.java
@@ -5,8 +5,19 @@ import lombok.Data;
 @Data
 public class User {
 
+  /**
+   * 用户id
+   */
   private Long id;
+
+  /**
+   * 用户名字
+   */
   private String name;
+
+  /**
+   * 用户头像地址
+   */
   private String head;
 
 }
