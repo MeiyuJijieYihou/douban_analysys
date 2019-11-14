dd6b283accdc9e701a6c79cfd898b75678d0f65d
DX <184606905@qq.com>
2019-11-14 21:21:04 +0800

    给音乐评论、书籍添加@ApiModel、@ApiModelProperty注解，便于观察

--- src/main/java/com/yiguan/douban/pojo/CommentMusicPojo.java
+++ src/main/java/com/yiguan/douban/pojo/CommentMusicPojo.java
@@ -1,5 +1,7 @@
 package com.yiguan.douban.pojo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.Date;
@@ -9,30 +11,36 @@ import java.util.Date;
  */
 
 @Data
+@ApiModel("音乐评论")
 public class CommentMusicPojo {
 
     /**
      * 音乐别名
      */
+    @ApiModelProperty("音乐别名")
     private String name;
 
     /**
      * 歌手名
      */
+    @ApiModelProperty("歌手名")
     private String singer;
 
     /**
      * 流派风格
      */
+    @ApiModelProperty("流派风格")
     private String style;
 
     /**
      * 发行时间
      */
+    @ApiModelProperty("发行时间")
     private Date issueDate;
 
     /**
      * 评论数目
      */
+    @ApiModelProperty("评论数目")
     private Integer reviewNum;
 }
