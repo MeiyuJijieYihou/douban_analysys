9d7f932f1dac9baec66ca5f04b04852a72a4c800
Chris_Nai <201750080522@stu.csust.edu.cn>
2019-11-14 20:38:49 +0800

    用户与地址实体类添加swagger注释

--- src/main/java/com/yiguan/douban/entity/User.java
+++ src/main/java/com/yiguan/douban/entity/User.java
@@ -1,26 +1,39 @@
 package com.yiguan.douban.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.persistence.Id;
 
+/**
+ * @Author: naipan
+ * @Date: 2019/11/14
+ * @Class_name: User
+ * @Qualified_ClassName:
+ * @Description: ToDo
+ */
 @Data
+@ApiModel("用户")
 public class User {
 
   /**
    * 用户id
    */
   @Id
+  @ApiModelProperty("用户id")
   private String id;
 
   /**
    * 用户名字
    */
+  @ApiModelProperty("用户名name")
   private String name;
 
   /**
    * 用户头像地址
    */
+  @ApiModelProperty("用户头像")
   private String head;
 
 }
