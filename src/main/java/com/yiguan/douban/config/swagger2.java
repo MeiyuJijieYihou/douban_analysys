7193ba0bda78547730083f26ff8364e6e2dbfc20
chenfule <1195289668@qq.com>
2019-11-16 21:07:57 +0800

    规范Swagger类的名字

--- src/main/java/com/yiguan/douban/config/swagger2.java
+++ src/main/java/com/yiguan/douban/config/swagger2.java
@@ -21,17 +21,17 @@ class Swagger2 {
     @Bean
     public Docket createRestApi(){
         return new Docket(DocumentationType.SWAGGER_2)
-            .apiInfo(apiInfo())
-            .select()
+                .apiInfo(apiInfo())
+                .select()
                 .apis(RequestHandlerSelectors.basePackage("com.yiguan.douban"))
-            .paths(PathSelectors.any())
-            .build();
+                .paths(PathSelectors.any())
+                .build();
     }
-     private ApiInfo apiInfo(){
+    private ApiInfo apiInfo(){
         return new ApiInfoBuilder()
                 .title("易观-大数据-豆瓣分析")
                 .description("基于豆瓣网站内容分析当前热门电影、音乐、书籍的类型和用户偏好。通过该项目学习数据抓取和用户分析。")
                 .version("1.0")
-           .build();
-     }
+                .build();
+    }
 }
