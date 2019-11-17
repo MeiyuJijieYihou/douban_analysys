bf3f8bde059530bfcce215c71c6a46abe145f20c
FuncJ <2419644024@qq.com>
2019-11-17 23:43:07 +0800

    实现根据音乐id得到最新的N条音乐评论，N由前端传入，默认为10

---
+++ src/main/java/com/yiguan/douban/pojo/MusicNewCommentPojo.java
@@ -0,0 +1,12 @@
+package com.yiguan.douban.pojo;
+
+/**
+ * @version v1.0
+ * @ProjectName: douban
+ * @ClassName: MusicNewCommentPojo
+ * @Description: TODO(一句话描述该类的功能)
+ * @Author: Function
+ * @Date: 2019/11/17 22:17
+ */
+public class MusicNewCommentPojo {
+}
